2e425ed8700205697fe7d49fd15f7715de4e51e7
mihooshima <meishui1107@gmail.com>
2020-09-05 11:15:47 +0900

    再修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'items#index'
-  resources :users, only: [:edit, :update, :new, :create ]
+  root to: "items#index"
+  resources :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,12 +18,13 @@ ActiveRecord::Schema.define(version: 2020_08_27_120908) do
     t.string "family_name", null: false
     t.string "first_name_kana", null: false
     t.string "family_name_kana", null: false
-    t.string "birthday", default: "", null: false
+    t.date "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
