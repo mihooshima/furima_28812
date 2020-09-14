ec3115f6ab60fa7e8802eb6ffa332af953fc3534
mihooshima <meishui1107@gmail.com>
2020-09-14 21:05:30 +0900

    終了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
+  before_action :move_to_index, except: [:index, :create]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except:[:index]
+  
   def index
     @products = Product.all.order(created_at: :desc)
   end
@@ -21,9 +25,33 @@ class ProductsController < ApplicationController
      @product = Product.find(params[:id])
   end
 
-  private
+  def edit
+  end
+ 
+  def update
+    if @product.user_id == current_user.id
+      if @product.update(product_params)
+        redirect_to product_path(@product.id)
+      else
+        redirect_to action: :edit
+      end
+    else
+      redirect_to action: :edit
+    end
+  end
 
+  private
   def product_params
     params.require(:product).permit(:name, :explanation, :price, :category_id, :image, :shipping_id, :shipping_cost_id,:day_id, :products_status_id).merge(user_id: current_user.id)
   end
-end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+ 
+  def set_item
+    @product = Product.find(params[:id])
+  end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   #get 'articles/index'
   #get 'articles/new'
   root to: 'products#index'
-  resources :products
+  resources :products, only: [:index, :new, :create, :show, :edit, :update] do
+  end
 
 
   end
