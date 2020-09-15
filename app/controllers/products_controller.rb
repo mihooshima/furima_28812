9f9679a167a3ce9132d0aa4cecef9ed04a589ebf
mihooshima <meishui1107@gmail.com>
2020-09-15 15:11:13 +0900

    before_action3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action : @product,only:[:show,:destroy]
+  before_action :set_product ,only:[:show,:destroy]
   def index
     @products = Product.all.order(created_at: :desc)
   end
@@ -19,7 +19,6 @@ class ProductsController < ApplicationController
   end
 
   def show
-     @product = Product.find(params[:id])
   end
 
   def destroy
